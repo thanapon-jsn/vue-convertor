4821caa33ea11c50eb157f25d19882b58738d173
Thanapon Jaisanuk <thanapon.jsn@gmail.com>
2020-12-03 01:40:24 +0700

    init project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hello">
+    <p>Oracle field and data type</p>
+
+    <form>
+      <textarea v-model="textBox" placeholder="Add oracle field type" rows="5" cols="120"></textarea>
+      <br>
+      <button v-on:click="clickToConvert()">Convert</button>
+    </form>
+    <h1>{{ text }}</h1>
+  </div>
+</template>
+
+<script>
+import { convertOracleTypeToJava } from '../services/java-convertor'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      textBox: '',
+      text: ''
+    }
+  },
+  methods: {
+    clickToConvert: function () {
+     let fieldsJava = convertOracleTypeToJava(this.textBox);
+     fieldsJava.forEach(fieldJava => {
+       this.text += fieldJava + '\n';
+     });
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/services/java-convertor.js
@@ -0,0 +1,56 @@
+function convertOracleTypeToJava(textFields) {
+    var fields = spiltFields(textFields);
+    var resultJavaFields = [];
+
+     fields.forEach(field => {
+         let fieldDetail = getFieldNameAndType(field);
+
+         let fieldName = toCamelCase(fieldDetail[0]);
+         let fieldType = fieldDetail[1].replaceAll(/\s|,/g, "");
+
+         if (/VARCHAR2[(]\d{1,}[)]/gi.test(fieldType)) {
+            fieldType = "String";
+         } else if (/NUMBER[(]\d{1,}[)]/gi.test(fieldType)) {
+            fieldType = "Long";
+         } else if (/NUMBER[(]\d{1,},\d{1,}[)]/gi.test(fieldType)) {
+            fieldType = "BigDecimal";
+         } else if (/DATE|TIMESTAMP/gi.test(fieldType)) {
+            fieldType = "Date";
+         }
+         resultJavaFields.push("private " + fieldType + " " +  fieldName + ";");
+     });
+
+    return resultJavaFields;
+}
+
+function spiltFields(textFields) {
+    textFields = textFields.replace("\n", "")
+    return textFields.split(",");
+}
+
+function getFieldNameAndType(fieldData) {
+   let fieldsData = fieldData.split(/\s+/);
+   if (!fieldsData[0]) {
+    fieldsData.splice(0, 1);
+   }
+
+   if (!fieldsData[3]) {
+    fieldsData.splice(3, 1);
+   }
+   
+    return fieldsData;
+}
+
+function toCamelCase(str) {
+    if (str === void 0) { str = ''; }
+    if (!str)
+        return '';
+    return String(str)
+        .replace(/^[^A-Za-z0-9]*|[^A-Za-z0-9]*$/g, '')
+        .replace(/[^A-Za-z0-9]+/g, '$')
+        .replace(/([a-z])([A-Z])/g, function (m, a, b) { return a + "$" + b; })
+        .toLowerCase()
+        .replace(/(\$)(\w)/g, function (m, a, b) { return b.toUpperCase(); });
+}
+
+export { convertOracleTypeToJava }
